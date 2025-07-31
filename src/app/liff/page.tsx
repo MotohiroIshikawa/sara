537174758b198fd3d280ca44423ac8fdf25ed5fa
motohiro.ishikawa <motohiro.ishikawa@g.softbank.co.jp>
2025-07-31 12:44:50 +0900

    LIFF lineLoginなし 4

--- src/app/liff/page.tsx
+++ src/app/liff/page.tsx
@@ -1,34 +1,38 @@
 'use client';
 
 import { useLiff } from './liff-provider';
-import { Profile } from '@liff/get-profile';
+import { Profile as LineProfile } from '@liff/get-profile';
 import { useEffect, useState } from 'react';
 
 export default function Page() {
-  const [profile, setProfile] = useState<Profile | null>(null);
+  const [lineProfile, setProfile] = useState<LineProfile | null>(null);
   const { liffState } = useLiff();
 
   useEffect(() => {
     if (liffState?.isLoggedIn()) {
       (async () => {
-        const profile = await liffState.getProfile();
-        setProfile(profile);
+        const lineProfile = await liffState.getProfile();
+        setProfile(lineProfile);
       })();
     }
   }, [liffState]);
 
   return (
     <div>
-      {profile && (
+      {lineProfile && (
         <>
-          {/* eslint-disable-next-line @next/next/no-img-element */},
-          <img
-            src={profile.pictureUrl}
-            alt='profile'
-            className='rounded-full w-20 h-20 mx-auto mb-4'
-          />
-          <p className='text-center font-bold text-xl'>userId: {profile.userId}</p>
-          <p className='text-center text-gray-500'>displayName: {profile.displayName}</p>
+          <p>LIFF - No need to login after init to get user profile.</p>
+          <p classname="font-bold">UserProfile</p>
+          <p>userId: {lineProfile.userId}</p>
+          <p>displayName: {lineProfile.displayName}</p>
+          <p>
+            {/* eslint-disable-next-line @next/next/no-img-element */},
+            <img
+              src={lineProfile.pictureUrl}
+              alt='profile'
+              className='rounded-full w-20 h-20 mx-auto mb-4'
+            />
+          </p>
         </>
       )}
     </div>
