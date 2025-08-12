f60b0751e9d60ceca740ae20c78d59448b1ef116
motohiro.ishikawa <motohiro.ishikawa@g.softbank.co.jp>
2025-08-12 11:57:22 +0900

    API呼び出し時にbodyをJSON化

--- app/api/getOpenAI/route.ts
+++ app/api/getOpenAI/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { connectOpenAI } from "@/utils/connectOpenAI";
 
 export async function POST(request: Request) {
-  const { message } = await request.json();
-  const data = await connectOpenAI(message);
+  const { text } = await request.json();
+  const data = await connectOpenAI(text);
   return NextResponse.json(data);
 }
