354f902b3d8596dd29cd75265dcc2414fbe8327d
motohiro.ishikawa <motohiro.ishikawa@g.softbank.co.jp>
2025-09-27 20:15:45 +0900

    常駐化３

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,21 @@
+// ecosystem.config.cjs
 module.exports = {
   apps: [{
     name: "sara-stg",
     cwd: "/home/azureuser/sara",
-    script: "npm",
-    args: "run stg:start",
+    script: "node",
+    args: "server.mjs",
     env: {
       NODE_ENV: "production",
       APP_ENV: "stg",
       PORT: "3000",
       HOST: "0.0.0.0"
     },
-    exec_mode: "cluster",
+    exec_mode: "fork",
     instances: 1,
     watch: false,
     out_file: "/home/azureuser/pm2logs/sara-stg.out.log",
     error_file: "/home/azureuser/pm2logs/sara-stg.err.log",
-    merge_logs: true,
-    max_restarts: 10,
-    restart_delay: 2000
+    merge_logs: true
   }]
 };
